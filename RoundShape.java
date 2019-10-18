fa6b4af4356b52e85b60787fdb4002fef4d4b1cb
thomasm20 <thomasm20@mail.wlu.edu>
2019-10-18 18:12:01 -0400

    should be done

--- RoundShape.java
+++ RoundShape.java
@@ -10,15 +10,24 @@
 
 public abstract class RoundShape extends AllShapes{
   
-  
+  	protected int radius;
 
-  /**
+         /**
 	 * Returns the integer width of the Oval
 	 * @return width of the Oval
 	 */
 	public int getRadius()
 	{
 		return radius;
+	}
+	
+	/**
+	 * Sets width of the Oval to a new one
+	 * @param newWidth - new width of the Oval
+	 */
+	public void setRadius(int newRadius)
+	{
+		this.radius = newRadius;
 	}
   
   }
