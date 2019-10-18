61aa5244a829eef1c1cd66c6af501ba6c933f153
thomasm20 <thomasm20@mail.wlu.edu>
2019-10-18 17:54:21 -0400

    edit

--- StraightLineShape.java
+++ StraightLineShape.java
@@ -11,32 +11,11 @@
 
 public abstract class StraightLineShape extends AllShapes{
 
-	protected int[] xCoords;
-	protected int[] yCoords;
-
     /**
 	 * Returns the perimeter of the Shape
 	 * @return the perimeter as a double
 	 */
     public abstract double getPerimeter();
-
-    /**
-	 * Returns the int[] of xCoords of the Shape
-	 * @return integer array containing the x coordinates
-	 */
-	public int[] getXCoordinates()
-	{
-		return xCoords;
-	}
-
-    /**
-	 * Returns the int[] of yCoords of the Shape
-	 * @return integer array containing the y coordinates
-	 */
-	public int[] getYCoordinates()
-	{
-		return yCoords;
-	}
 	
 	/**
 	 * Returns a double[] with the sides of the shape
